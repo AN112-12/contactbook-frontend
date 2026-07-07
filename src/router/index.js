f702e63a69af32f49f9fc160447e4edf63d1a426
An Tran <khanhan11211@gmail.com>
2026-07-07 11:05:08 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import ContactBook from "@/views/ContactBook.vue";
 import { createRouter, createWebHistory } from "vue-router";
 const routes = [
-//   {
-//     path: "/",
-//     name: "contactbook",
-//     component: ContactBook,
-//   },
-    {
-        path: "/:pathMatch(.*)*",
-        name: "notfound",
-        component: () => import("@/views/NotFound.vue"),
-    },
+  {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
+{
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+},
+{
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true // Truyền các biến trong $route.params vào làm props
+},
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
